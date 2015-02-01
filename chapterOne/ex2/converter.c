da4e34a05551e1553f7fc05b10ce0ea59589201a
Shezad Khan <shezadkhan137@gmail.com>
2015-02-01 22:20:47 +0000

    Small improvements

--- chapterOne/ex2/converter.c
+++ chapterOne/ex2/converter.c
@@ -5,7 +5,7 @@
 
 int main ()
 {
-    int fahr, cels;
+    float fahr, cels;
     int lower, upper, step;
     lower = 0;
     upper = 300;
@@ -13,9 +13,12 @@ int main ()
 
     fahr = lower;
 
+    printf("Fahrenheit\t\tCelsius\n");
+    printf("===========\t\t======\n");
+
     while (fahr <= upper){
-        cels = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, cels);
+        cels = (5.0/9.0) * (fahr-32.0);
+        printf("%3.1f\t\t\t%3.1f\n", fahr, cels);
         fahr += step;
     }
     return 0;
